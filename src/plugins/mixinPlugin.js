973bb3d14c90063ceec9b6afce3579129be6e78b
yyd <609825025@qq.com>
2020-09-27 15:32:36 +0800

    修改

--- src/plugins/mixinPlugin.js
+++ src/plugins/mixinPlugin.js
@@ -5,6 +5,14 @@ import { resetVueData } from 'js/yydjs';
 export default {
     install() {
         Vue.mixin({
+            data() {
+                return {
+                    query: {},
+                };
+            },
+            onLoad(option) {
+                this.query = option;
+            },
             onShow() {
                 let needResetList = ['/pages/test/test'];
                 let { path } = this.$Router.$Route;
