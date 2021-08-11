a085e414c6132a47182d293ebd2f8e98518e3b3c
yyd <609825025@qq.com>
2021-08-11 18:16:37 +0800

    修改

--- src/store/index.js
+++ src/store/index.js
@@ -21,7 +21,6 @@ const state = {
     isLoading: false, //遮罩是否显示loading
     showRefreshBt: false, //遮罩是否显示刷新按钮
     status: '', //服务器错误状态
-    userInfo: {}, //用户信息
 };
 
 const store = new Vuex.Store({
